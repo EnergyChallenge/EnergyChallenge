145aa36141c51136e02cf3b0632777e7ccc9f539
wra <wolfgang.ramos@gmail.com>
2015-03-10 10:34:01 +0100

    added classes Profile and CompletedActivity; updated class User

--- Server/grails-app/domain/de/unikiel/klik/model/User.groovy
+++ Server/grails-app/domain/de/unikiel/klik/model/User.groovy
@@ -1,11 +1,15 @@
 package de.unikiel.klik.model
 
-class User {
+class User extends Profile {
 
+	String name
+	String email
+	ArrayList<CompletedActivity> completedActivities
+	
     static constraints = {
     }
 	
 	def completeAcitivty(Activity activity) {
-		
+		this.completedActivties.add(activity)
 	}
 }
