47c08b190a6f78bdaa5ee7355c57210850458dda
Marco Osterloh <mao@informatik.uni-kiel.de>
2015-03-11 11:13:08 +0100

    Update SeleniumGUITest, added time limitations for element load

--- Server/test/unit/de/unikiel/klik/SeleniumGUITest.java
+++ Server/test/unit/de/unikiel/klik/SeleniumGUITest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,6 +16,8 @@ public class SeleniumGUITest {
 	
 	//declare a driver to test the grails project in firefox browser
 	WebDriver driver = new FirefoxDriver();
+	//declare a maximum waiting time for specific elements
+	WebDriverWait driverWait = new WebDriverWait(driver, 5);
 
 	@Before
 	public void setUp() throws Exception {
@@ -21,33 +25,33 @@ public class SeleniumGUITest {
 		driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
 		//extend browser window to maximum
 		driver.manage().window().maximize();
-		//open starting page URL
-		driver.get("http://localhost:8080/Server/");
 	}
 
 	@Test
 	public void test() {
 		
+		//open starting page URL
+		driver.get("http://localhost:8080/Server/");
 		//check if current URL is correct
 		String currentUrl = driver.getCurrentUrl();
 		String startExpectedUrl = "http://localhost:8080/Server/";
 		assert currentUrl.equals(startExpectedUrl);
 		
 		//perform a login
-		//choose the text input field for the user name and put in "user"
-		WebElement element = driver.findElement(By.name("username"));
+		//choose the text input field for the user name if it is available on time and type "user"
+		WebElement element = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.name("username")));
 		element.sendKeys("user");
 		
 		//select the input field for the password and put in "password"
-		element = driver.findElement(By.name("password"));
+		element = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.name("password")));;
 		element.sendKeys("password");
 		
 		//select and press the login button
-		element = driver.findElement(By.cssSelector("input[value='Sign in']"));
+		element = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[value='Sign in']")));
 		element.click();
 		
 		//navigate to "Aktivitaeten" page by selecting and pressing the button
-		element = driver.findElement(By.linkText("Aktivitaeten"));
+		element = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Aktivitaeten")));
 		element.click();
 		//check if GUI really redirected to "Aktivitaeten"
 		currentUrl = driver.getCurrentUrl();
@@ -55,8 +59,10 @@ public class SeleniumGUITest {
 		assert currentUrl.equals(activityExpectedUrl);
 		
 		//perform a logout
-		element = driver.findElement(By.cssSelector("input[value='Logout']"));
+		element = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[value='Logout']")));
 		element.click();
+		
+		//check if logout worked by checking the current URL
 		currentUrl = driver.getCurrentUrl();
 		assert currentUrl.equals(startExpectedUrl);
 	}
