02f08da4f39bae3de572969b8ffcd62465ec4949
Wolfgang Ramos <wolfgang.ramos@gmail.com>
2015-03-11 18:20:00 +0100

    updated spock test for domain class KlikUser

--- Server/grails-app/domain/de/unikiel/klik/model/KlikUser.groovy
+++ Server/grails-app/domain/de/unikiel/klik/model/KlikUser.groovy
@@ -7,13 +7,21 @@ class KlikUser extends Profile {
 	static hasOne = [team: Team]
 	
     static constraints = {
-		//email nullable: false
-		completedActivities nullable: true 
+		completedActivities(nullable: true) 
     }
-		
-	def completeAcitivityNow(Activity activity) {
+	
+	
+	def completeActivityNow(Activity activity) {
 		def completedActivity = new CompletedActivity(activity: activity, date: new Date())
 		this.addToCompletedActivities(completedActivity)
 	}
 	
+	
+	def int countPoints() {
+		int sum = 0
+		for(completedActivity in completedActivities) {
+			sum += completedActivity.getActivity().getPoints()
+		}
+		return(sum)
+	}
 }
--- Server/test/unit/de/unikiel/klik/model/KlikUserSpec.groovy
+++ Server/test/unit/de/unikiel/klik/model/KlikUserSpec.groovy
@@ -9,21 +9,39 @@ import spock.lang.*
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 //@TestFor(KlikUserController)
-@Mock(KlikUser)
+@Mock([KlikUser, Activity, CompletedActivity])
 class KlikUserSpec extends Specification {
 
-    def setup() {
+    def setup() { // executed before each feature method
+		// create fixture for all feature methods
     }
 
-    def cleanup() {
+    def cleanup() { // executed after each feature method
     }
 
-	@Ignore
-    void "email should contain uni-kiel.de"() {
-		setup:
+    def "completing an activity should add it to completed activities and increase the users points"() {
+		setup: "create a user and an activity"
+		// create fixture for THIS test
 		def testUser = new KlikUser(name: "Wolfgang", email: "wolfgang@mail.uni-kiel.de")
+		def activity = new Activity(title: "Fahrradfahren", points: 3)
 		
-		expect:
-		testUser.getEmail().contains("uni-kiel.de")
+		expect: "new users have no points"
+		testUser.countPoints() == 0
+		
+		when: "user completes activity once"
+		testUser.completeActivityNow(activity)
+		
+		then: "activity is added to set of completed activities"
+		testUser.getCompletedActivities().size() == 1
+		testUser.getCompletedActivities().toArray(new CompletedActivity[0])[0].getActivity() == activity
+		
+		and: "activity points are added to the users points"
+		testUser.countPoints() == activity.getPoints()
+		
+		when: "activity is completed twice"
+		testUser.completeActivityNow(activity)
+		
+		then: "users points should double"
+		testUser.countPoints() == 2*activity.getPoints()
     }
 }
