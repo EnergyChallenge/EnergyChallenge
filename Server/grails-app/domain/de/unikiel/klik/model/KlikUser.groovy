c3bb8c80a68caba34367479cdbc298ee7858eed3
Lennard Hammer <stu115635@mail.uni-kiel.de>
2015-03-11 15:35:19 +0100

    disabled Team because it did not work :(

--- Server/grails-app/domain/de/unikiel/klik/model/KlikUser.groovy
+++ Server/grails-app/domain/de/unikiel/klik/model/KlikUser.groovy
@@ -2,13 +2,15 @@ package de.unikiel.klik.model
 
 class KlikUser extends Profile {
 
+	//Team team;
 	//String email; // is in ShiroUser
 	static hasMany = [completedActivities: CompletedActivity]
-	static hasOne = [team: Team]
+	
 	
     static constraints = {
 		//email nullable: false
 		completedActivities nullable: true 
+		//team nullabel: true
     }
 		
 	def completeAcitivityNow(Activity activity) {
