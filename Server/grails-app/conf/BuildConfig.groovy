6ae75547f3aea545f9c9573a57c01be01edbe7e7
Lennard Hammer <stu115635@mail.uni-kiel.de>
2015-03-16 09:48:02 +0100

    include Joda-Time

--- Server/grails-app/conf/BuildConfig.groovy
+++ Server/grails-app/conf/BuildConfig.groovy
@@ -62,6 +62,7 @@ grails.project.dependency.resolution = {
         compile ':cache:1.1.8'
         compile ":asset-pipeline:1.9.9"
 		compile ":shiro:1.2.1"
+		compile ":joda-time:1.5"
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
