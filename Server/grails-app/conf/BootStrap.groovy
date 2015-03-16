b06ab36e3a4e28152b171767a1b3b939f2d606e8
Lennard Hammer <stu115635@mail.uni-kiel.de>
2015-03-16 14:10:07 +0100

    new Default user: user@example.com / pass: password

--- Server/grails-app/conf/BootStrap.groovy
+++ Server/grails-app/conf/BootStrap.groovy
@@ -1,15 +1,22 @@
+import de.unikiel.klik.model.Institute
 import de.unikiel.klik.model.Role
 import de.unikiel.klik.model.User
 import org.apache.shiro.crypto.hash.Sha256Hash
 class BootStrap {
 
     def init = { servletContext ->
+		def TestService
 		def userRole = new Role(name: "user")
 		userRole.addToPermissions("*:*")
 		userRole.save();
-		def user = new User(email: "user@example.com", passwordHash: new Sha256Hash("password").toHex(), name: "Max Mustermann")
+		Institute institute = new Institute(name: "none")
+		institute.save()
+		def user = new User(email:"user@example.com", passwordHash: new Sha256Hash("password").toHex(), firstName: "Max", lastName: "Mustermann", institute: institute)
         user.addToRoles(userRole)
-		user.save()
+		if(!user.save()) {
+			println "saveFailed"
+		}
+		
     }
     def destroy = {
     }
