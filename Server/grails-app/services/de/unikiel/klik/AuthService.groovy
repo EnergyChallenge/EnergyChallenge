1bb5fde439b7cdcd7e53a0e6770cf9c1fe1dd10b
Lennard Hammer <stu115635@mail.uni-kiel.de>
2015-03-16 11:07:54 +0100

    Fixed Login

--- Server/grails-app/services/de/unikiel/klik/AuthService.groovy
+++ Server/grails-app/services/de/unikiel/klik/AuthService.groovy
@@ -1,16 +1,25 @@
 package de.unikiel.klik
 
 import grails.transaction.Transactional
+import org.apache.shiro.SecurityUtils
+import org.apache.shiro.authc.AuthenticationException
+import org.apache.shiro.authc.UsernamePasswordToken
 import org.apache.shiro.subject.Subject
 
 @Transactional
 class AuthService {
 
-    void logIn(String email, String password) {
-
+	def shiroSecurityManager
+	
+    void login(String email, String password, boolean rememberMe) throws AuthenticationException {
+		def authToken = new UsernamePasswordToken(email, password)
+		authToken.rememberMe = rememberMe
+		SecurityUtils.subject.login(authToken)
     }
-	void logOut (Subject subject) {
-		
+	void logout (Subject subject) {
+		// Log the user out of the application.
+		subject.logout()
+		//webRequest.getCurrentRequest().session = null
 	}
 	void register (String email, String password, String password2,String institute) {
 		
