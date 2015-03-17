c2c05d52bd7ef8dcc35404ec522685707ea3bd28
Samuel Barnes <stu121320@mail.uni-kiel.de>
2015-03-17 15:12:39 +0100

    Adding initial message service implimentation

--- Server/grails-app/services/de/unikiel/klik/MessageService.groovy
+++ Server/grails-app/services/de/unikiel/klik/MessageService.groovy
@@ -6,13 +6,40 @@ import org.apache.shiro.subject.Subject
 @Transactional
 class MessageService {
 
-    void inviteUserToTeam(long recieverId, Subject subject) {
+    void inviteUserToTeam(long receiverId, Subject subject) {
 
+        //Get the user the message belongs to via the id
+        User receiver = User.findById(recieverId)
+
+        //Get the sender via subject
+        User sender = User.findByEmail(subject.getPrincipal())
+
+        //Create the invitation
+        TeamInvite invitation = new TeamInvite(receiver: receiver, sender: sender)
+        invitation.save()
     }
+
 	void remindUser(long reciverId) {
-		
+
+        //Get the user the reminder belongs to via the id
+        User receiver = User.findById(recieverId)
+
+        //Create the reminder
+        ActivityNotification activityNotification = new ActivityNotification(receiver: receiver)
+        activityNotification.save()
 	}
+
 	void remindUser(long reciverId, long activityId) {
-		
+
+        //Get the user the reminder belongs to via the id
+        User receiver = User.findById(recieverId)
+
+        //Get the user the reminder belongs to via the id
+        Activity specificActivity = Activity.findById(activityId)
+
+        //TODO No facility in SpecificActivityNotification for passing the specific activity
+        //Create the reminder
+        SpecificActivityNotification specificActivityNotification = new SpecificActivityNotification(receiver: receiver)
+        specificActivityNotification.save()
 	}
 }
