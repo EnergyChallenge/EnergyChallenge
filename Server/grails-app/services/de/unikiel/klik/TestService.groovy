0ee0c7733a14ee68bd978702242f346c19ea9dac
Lennard Hammer <stu115635@mail.uni-kiel.de>
2015-03-17 13:14:16 +0100

    TestSevice is now able to add some Data to dataBase

--- Server/grails-app/services/de/unikiel/klik/TestService.groovy
+++ Server/grails-app/services/de/unikiel/klik/TestService.groovy
@@ -9,29 +9,64 @@ import org.joda.time.Duration
 class TestService {
 
 	static Activity getExampleActivity() {
-		return new Activity(description: "Example", points: 1, duration: new Duration(60*60*1000))
+		return getExampleActivity("Example")
+	}
+	static Activity getExampleActivity(String name) {
+		return new Activity(description: name, points: 1, duration: new Duration(60*60*1000))
 	}
 	static Institute getExampleInstitute() {
-		return new Institute(name: "Example")
+		Institute institute
+		if(Institute && Institute.findByName("Example")) {
+			institute = Institute.findByName("Example")
+		}else {
+		institute = new Institute(name: "Example")
+		}
+		
+		return institute
 	}
 	static Proposal getExampleProposal() {
-		return new Proposal(description: "Example", points: 1, author: getExampleUser("ProposalAuthor"))
+		return getExampleProposal("Example")
+	}
+	static Proposal getExampleProposal(String name) {
+		return new Proposal(description: name, points: 1, author: getExampleUser("ProposalAuthor"))
 	}
 	static Team getExampleTeam() {
 		return getExampleTeam("Example")
 	}
 	static Team getExampleTeam(String name) {
-		return new Team(name: name, members:[getExampleUser(name+"Member1"),getExampleUser(name+"Member2")])
+		return new Team(name: name, members:[
+			getExampleUser(name+"Member1"),
+			getExampleUser(name+"Member2")
+		])
 	}
 	static Role getExampleRole() {
-		def role = new Role(name: "user")
-		role.addToPermissions("*:*")
+		def role
+		if(Role && Role.findByName("user")) {
+			role = Role.findByName("user")
+		}
+		else {
+			role = new Role(name: "user")
+			role.addToPermissions("*:*")
+		}
+
 		return role
 	}
-    static User getExampleUser() {
+	static User getExampleUser() {
 		return getExampleUser("Example")
-    }
+	}
 	static User getExampleUser(String name) {
 		return new User(email:name.toLowerCase()+"@example.com", passwordHash: new Sha256Hash("password").toHex(), roles: [getExampleRole()], firstName: name, lastName: "Example", institute: getExampleInstitute())
+	}
+	static void saveSomeExampleData(){
+		getExampleInstitute().save()
+		getExampleUser("User1").save()
+		getExampleUser("User2").save()
+		getExampleTeam("Team1").save()
+		getExampleTeam("Team2").save()
+		getExampleTeam("Team3").save()
+		getExampleActivity("Activity1").save()
+		getExampleActivity("Activity2").save()
+		getExampleProposal("Proposal1").save()
+		getExampleProposal("Proposal2").save()
 	}
 }
