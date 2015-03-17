a8a7ba0f8e9a988ce3dcc723dd88cb928dcc2818
Lennard Hammer <stu115635@mail.uni-kiel.de>
2015-03-17 13:18:10 +0100

    BootStrap support for different enviroments

--- Server/grails-app/conf/BootStrap.groovy
+++ Server/grails-app/conf/BootStrap.groovy
@@ -1,23 +1,36 @@
+import de.unikiel.klik.TestService
 import de.unikiel.klik.model.Institute
 import de.unikiel.klik.model.Role
+import de.unikiel.klik.model.Team
 import de.unikiel.klik.model.User
 import org.apache.shiro.crypto.hash.Sha256Hash
+import grails.util.Environment
+
+
 class BootStrap {
 
-    def init = { servletContext ->
-		def TestService
-		def userRole = new Role(name: "user")
-		userRole.addToPermissions("*:*")
-		userRole.save();
-		Institute institute = new Institute(name: "none")
-		institute.save()
-		def user = new User(email:"user@example.com", passwordHash: new Sha256Hash("password").toHex(), firstName: "Max", lastName: "Mustermann", institute: institute)
-        user.addToRoles(userRole)
-		if(!user.save()) {
-			println "saveFailed"
+	def init = { servletContext ->
+		def currentEnv = Environment.current
+
+		if (currentEnv == Environment.DEVELOPMENT) {
+
+			def userRole = new Role(name: "user")
+			userRole.addToPermissions("*:*")
+			userRole.save();
+			Institute institute = new Institute(name: "none")
+			institute.save()
+			def user = new User(email:"user@example.com", passwordHash: new Sha256Hash("password").toHex(), firstName: "Max", lastName: "Mustermann", institute: institute)
+			user.addToRoles(userRole)
+			user.save()
+			//TestService.saveSomeExampleData()
+			println "Running in Development Mode"
+		} else if (currentEnv == Environment.TEST) {
+			// do custom init for test here
+
+		} else if (currentEnv == Environment.PRODUCTION) {
+			// do custom init for prod here
 		}
-		
-    }
-    def destroy = {
-    }
+	}
+	def destroy = {
+	}
 }
