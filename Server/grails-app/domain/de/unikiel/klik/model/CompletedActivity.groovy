553cbf236a256cf611d40e7004f46ff84db4101f
wra <wolfgang.ramos@gmail.com>
2015-03-18 12:13:15 +0100

    added code for db mapping

--- Server/grails-app/domain/de/unikiel/klik/model/CompletedActivity.groovy
+++ Server/grails-app/domain/de/unikiel/klik/model/CompletedActivity.groovy
@@ -13,4 +13,10 @@ class CompletedActivity {
 		dateCreated(nullable: false, defaultValue: "DateTime.now()")
     }
 	
+	// TODO create point property for completed activities; check db mapping
+	//static mapping = {
+	//		table 'completed_activity'
+	//		activity column: 'activity_id'
+	//		points formula: "(SELECT points FROM Activity a WHERE a.id = activity_id"
+	//}
 }
