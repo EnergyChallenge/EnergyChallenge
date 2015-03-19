7d4b21ddeecb68b1799be4d06dab95aed603c4a9
Samuel Barnes <stu121320@mail.uni-kiel.de>
2015-03-19 12:39:28 +0100

    Adding initial app controller

--- Server/grails-app/controllers/de/unikiel/klik/AppController.groovy
+++ Server/grails-app/controllers/de/unikiel/klik/AppController.groovy
@@ -1,49 +1,135 @@
 package de.unikiel.klik
 
 import grails.converters.JSON
+import de.unikiel.klik.model.User;
+import de.unikiel.klik.model.Team;
+import de.unikiel.klik.model.Activity;
+import de.unikiel.klik.model.Proposal;
+import de.unikiel.klik.model.Institute;
+import org.apache.shiro.SecurityUtils;
+import java.util.ArrayList;
 
 class AppController {
 
-    def index() { 
-		def test = [["Hallo"],["Welt"]];
-		
-		render test as JSON;
+    def index() {
 	}
+
+    def getRankingUsers() {
+
+        //Get the appropriate information from the users
+        for (user in User.findAll()) {
+            rankedUsers = [name: user.getName(), id: user.id, points: user.getPoints()];
+        }
+
+        //Sort the users
+        rankedUsers.sort { -it.points }
+
+        //Return them as a JSON
+        render rankedUsers as JSON;
+    }
 	
 	def getRankingTeams() {
-		
-		TimeZone reference = TimeZone.getTimeZone("GMT");
-		Calendar myCal = Calendar.getInstance(reference);
-		def currentTime = myCal.getTime();
-		
-		
-		def team1 = [
-			name : "Team 1",
-			punkte : 2048];
-		
-		def team2 = [
-			name : "Team 2",
-			punkte : 2048];
-		
-		def team3 = [
-			name : "Team 3",
-			punkte : 512];
-		
-		def team4 = [
-			name : "Team 4",
-			punkte : 256];
-		
-		def team5 = [
-			name : "CurrentTime Team - " + currentTime,
-			punkte : 128];
-		
-		def ranking = [team1, team2, team3, team4, team5];
-		
-		def rankingRequest = [
-			optionales : "Optional weitere Daten",
-			ranking : ranking];
-		
-		render rankingRequest as JSON;
-		
+
+        //Get the appropriate information from the teams
+        for (team in Team.findAll()) {
+            rankedTeams = [name: team.getName(), id: team.id, points: team.getPoints()];
+        }
+
+        //Sort the teams
+        rankedTeams.sort { -it.points }
+
+        //Return them as a JSON
+        render rankedTeams as JSON;
 	}
+
+    def getUserProfile(){
+
+        //Get the user to be returned
+        User user;
+        if (params.id != null) {
+            user = User.get(params.id);
+        } else {
+            user = User.findByEmail(SecurityUtils.getSubject().getPrincipal());
+        }
+
+        //Return the full object in JSON
+        render user as JSON;
+    }
+
+    def getTeamProfile(){
+
+        //Get the team to be returned
+        Team team = Team.get(params.id);
+
+        //Return the full object in JSON
+        render team as JSON;
+    }
+
+    def getActivities(){
+
+        //Get all the activities
+        def activities = Activity.getAll();
+
+        //Return the full objects in JSON
+        render activities as JSON;
+    }
+
+    def getFavoriteActivities(){
+
+        //Get the user and then retrive their favorites
+        User user;
+        if (params.id != null) {
+            user = User.get(params.id);
+        } else {
+            user = User.findByEmail(SecurityUtils.getSubject().getPrincipal());
+        }
+        def favorites = user.getFavorites();
+
+        //Return the favorites in JSON
+        render favorites as JSON;
+    }
+
+    def getProposals(){
+
+        //Get all the proposals
+        def proposals = Proposal.getAll();
+
+        //Return the full objects in JSON
+        render proposals as JSON;
+    }
+
+    def getProposal(){
+
+        //Get the proposal to be returned
+        Proposal proposal = Proposal.get(params.id);
+
+        //Return the full object in JSON
+        render team as JSON;
+    }
+
+    def search(){
+
+        //Get all users and teams matching the name given as the search parameter
+        //TODO I understand this isn't a great way to implement search (if this even works), any ideas?
+        Team team = Team.getByName(params.searchQuery);
+        User user = User.getByName(params.searchQuery);
+        def search;
+        search.add(user);
+        search.add(team);
+
+        //Return them as JSON
+        render search as JSON;
+    }
+
+    def doActivity(){
+
+        //Get the activity controller to complete an activity
+        ActivityService.completeActivity(params.activityId, SecurityUtils.getSubject())
+    }
+
+    def commentProposal(){
+
+        //Get the proposal service to add the comment
+        ProposalService.addComment(params.commentText, params.rating, SecurityUtils.getSubject(), params.proposalId);
+    }
 }
