bab1c1218a5806f31191874e28b1708c8680f88e
wra <wolfgang.ramos@gmail.com>
2015-03-20 15:28:19 +0100

    sorting activities by popularity is now possible

--- Server/grails-app/controllers/de/unikiel/klik/StatisticsController.groovy
+++ Server/grails-app/controllers/de/unikiel/klik/StatisticsController.groovy
@@ -1,6 +1,6 @@
 package de.unikiel.klik;
 
-import de.unikiel.klik.model.PageView;
+import de.unikiel.klik.model.*
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import grails.converters.JSON;
@@ -12,21 +12,35 @@ import org.grails.plugins.csv.CSVWriter
 
 class StatisticsController {
 
+	// used for SQL Queries
+	def dataSource
+	
     def index() { 
-		def mostPopularActivitys = getMostPopularActivitys();
-		def pageVisitsIndex = getVisitsOf("/",10)  
-		[mostPopularActivitys : mostPopularActivitys as JSON, pageVisitsIndex: pageVisitsIndex as JSON];
+		def mostPopularActivities = getMostPopularActivities();
+		def pageVisitsIndex = [1,2,3,4,5]	// TODO fix visitor counts 
+		[mostPopularActivitys : mostPopularActivities as JSON, pageVisitsIndex: pageVisitsIndex as JSON];
 	}
 
 	def exportCsv() {
 	
+	// query
+	groovy.sql.Sql sql = new groovy.sql.Sql(dataSource)
+	log.info(sql)
+	log.info("datasource " + dataSource)
+	def results = sql.rows("SELECT a.description AS description, counts.n AS n\n" +
+	"FROM activity a, (SELECT activity_id, count(*) AS n FROM completed_activity GROUP BY activity_id) counts\n" +
+	"WHERE a.id = counts.activity_id\n" +
+	"ORDER BY n DESC")
+	sql.close()
+	
 	def sw = new StringWriter()
 	def b = new CSVWriter(sw, {
-		col1 { it.val1 }
-		col2 { it.val2 }
+		col1:"description" { it.DESCRIPTION }
+		col2:"haeufigkeit" { it.N }
 		})
-	b << [val1: 'a', val2: 'b']
-	b << [val1: 'c', val2: 'd']
+	for(int i=0; i < results.size(); i++) {
+		b << results[i]
+	}
 
 	byte[] bytes = b.writer.toString().bytes
 
@@ -38,9 +52,11 @@ class StatisticsController {
 	render(view: "index")
 	}
 
-  private def getMostPopularActivitys(){
-    return [["Haendewaschen mit kaltem Wasser",10],["Mit dem Fahrrad zur Uni fahren bis 3km",20],["Persönlichen CO2-Abdruck berechnen",15],["Treppe statt Fahrstuhl nutzen pro Tag",2],["Mit dem Fahrrad zur Uni fahren bis 5km",12],["Mit dem Fahrrad zur Uni fahren über 5km",7]]
-  }
+	private def getMostPopularActivities(){
+		return [["Haendewaschen mit kaltem Wasser",10],["Mit dem Fahrrad zur Uni fahren bis 3km",20],["Persönlichen CO2-Abdruck berechnen",15],["Treppe statt Fahrstuhl nutzen pro Tag",2],["Mit dem Fahrrad zur Uni fahren bis 5km",12],["Mit dem Fahrrad zur Uni fahren über 5km",7]]
+	}
+	
+	/* TODO fix visit counts
   private def getVisitsOf(String url, int maxData){
     def rawData = PageView.findAll("from PageView as pageView where pageView.url=? order by pageView.dateCreated", [url], [max:maxData])
     def data = [];
@@ -50,4 +66,5 @@ class StatisticsController {
     }
     return data
   }
+  */
 }
