14d9ff444565f20abd3efb8895196c90fe800200
Sören Henning <stu114708@informatik.uni-kiel.de>
2015-03-24 16:06:45 +0100

    Login required

--- app/EnergyChallenge/app/src/main/java/de/unikiel/klik/energychallenge/Config.java
+++ app/EnergyChallenge/app/src/main/java/de/unikiel/klik/energychallenge/Config.java
@@ -2,14 +2,14 @@ package de.unikiel.klik.energychallenge;
 
 public class Config {
 
-    //public static final String SERVER_REST_PATH = "http://192.168.0.83:8080/Server/App/";
+    public static final String SERVER_REST_PATH = "http://192.168.0.2:8080/Server/App/";
     //public static final String SERVER_REST_PATH = "http://soerenhenning.de/Softwareprojekt/App/";
-    public static final String SERVER_REST_PATH = "http://luedtkegmbh.de/Soeren/Softwareprojekt/App/";
+    //public static final String SERVER_REST_PATH = "http://luedtkegmbh.de/Soeren/Softwareprojekt/App/";
 
     //TODO Remove
     public static final String SERVER_REST_PATH_END = "";
     //public static final String SERVER_REST_PATH_END = ".json";
 
-    public static final boolean LOGIN_REQUIRED = false;
+    public static final boolean LOGIN_REQUIRED = true;
 
 }
