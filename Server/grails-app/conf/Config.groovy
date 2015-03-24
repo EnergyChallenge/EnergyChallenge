af03834edd04e5f407acd69edcdfb647abd524ff
Samuel Barnes <stu121320@mail.uni-kiel.de>
2015-03-24 12:45:37 +0100

    Adding working global email service (settings for a gmail account currently work)

--- Server/grails-app/conf/Config.groovy
+++ Server/grails-app/conf/Config.groovy
@@ -68,6 +68,18 @@ grails {
     }
 }
 
+grails {
+    mail {
+        host = "smtp.gmail.com"
+        port = 465
+        username = "xxxxx@gmail.com"
+        password = "xxxxx"
+        props = ["mail.smtp.auth":"true",
+                 "mail.smtp.socketFactory.port":"465",
+                 "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
+                 "mail.smtp.socketFactory.fallback":"false"]
+    }
+}
 
 grails.converters.encoding = "UTF-8"
 // scaffolding templates configuration
