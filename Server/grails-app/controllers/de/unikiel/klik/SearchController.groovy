1fd5c07f1364a0dc85fddf1efc24af82553f1881
wra <wolfgang.ramos@gmail.com>
2015-03-24 15:06:45 +0100

    improved display of search results; results are now links

--- Server/grails-app/controllers/de/unikiel/klik/SearchController.groovy
+++ Server/grails-app/controllers/de/unikiel/klik/SearchController.groovy
@@ -7,7 +7,21 @@ class SearchController {
 	static defaultAction = "displaySearchResults"
 	
     def displaySearchResults() {
-		def results = User.findAllBySearchNameIlike("%" + params.query + "%")
+    
+		def results = [];
+		def matchingUsers = User.findAllBySearchNameIlike("%" + params.query + "%")
+		for (user in matchingUsers) {
+			results << [id: user.getId(), name: user.getName(),
+						type: "user", points: user.getPoints()];
+		}
+		def matchingTeams = Team.findAllByNameIlike("%" + params.query + "%")
+		for (team in matchingTeams) {
+			results << [id: team.getId(), name: team.getName(),
+						type: "team", points: team.getPoints()];
+		}
+		results.sort { -it.points } //Sort DESC
+		
+		//def results = User.findAllBySearchNameIlike("%" + params.query + "%")
 		
 		render(view: "displaySearchResults", model: [query: params.query, results: results])
     }
