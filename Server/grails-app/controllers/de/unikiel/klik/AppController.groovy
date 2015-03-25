07ab496b2c1d8108bafa72e950b11ff617a1f0e9
Sören Henning <stu114708@informatik.uni-kiel.de>
2015-03-26 00:37:36 +0100

    Resolve mysterious conflict

--- Server/grails-app/controllers/de/unikiel/klik/AppController.groovy
+++ Server/grails-app/controllers/de/unikiel/klik/AppController.groovy
@@ -26,7 +26,7 @@ class AppController {
 	def login() {
 		authenticate(params.email, params.password);
 		User user = User.findByEmail(SecurityUtils.getSubject().getPrincipal());
-		outputToJson({user: [id: user.id, name: user.getName()]});
+		outputToJson([user: [id: user.id, name: user.getName()]]);
 	}
 	
 	def userProfile() {
@@ -223,6 +223,9 @@ class AppController {
 		//TODO
 	}
 	
+	def avatar() {
+		redirect(controller: "profile", action: "avatar", id: params.id)
+	}
 	
 	
 	private void authenticate(String email, String password) {
@@ -233,13 +236,14 @@ class AppController {
 		}
 		catch (AuthenticationException ex){
 			//Login failed
-			def response = [authentication: false]
+			def response = [authentication: false, result: [loginFailed: true]]
 			render response as JSON
 		}
 	}
 	
 	private void outputToJson(def data) {
-		render (authentication: true, result: data) as JSON;
+		def response = [authentication: true, result: data]
+		render response as JSON;
 	}
 	
 	
