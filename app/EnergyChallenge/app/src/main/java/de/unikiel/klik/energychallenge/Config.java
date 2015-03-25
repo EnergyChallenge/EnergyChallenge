4ab2ee4b395d6f9a5878a8cec0afb7fd730b82f4
Sören Henning <stu114708@informatik.uni-kiel.de>
2015-03-25 22:30:53 +0100

    Update the ServerRequest Class

--- app/EnergyChallenge/app/src/main/java/de/unikiel/klik/energychallenge/Config.java
+++ app/EnergyChallenge/app/src/main/java/de/unikiel/klik/energychallenge/Config.java
@@ -3,9 +3,9 @@ package de.unikiel.klik.energychallenge;
 public class Config {
 
     //public static final String SERVER_REST_PATH = "http://192.168.0.3:8080/Server/App/"; //Nick Notebook
-    public static final String SERVER_REST_PATH = "http://192.168.0.83:8080/Server/App/"; //Lennard Notebook
-    //public static final String SERVER_REST_PATH = "http://luedtkegmbh.de/Soeren/Softwareprojekt/App/"; //Thomas Lüdtke
+    //public static final String SERVER_REST_PATH = "http://192.168.0.83:8080/Server/App/"; //Lennard Notebook
+    public static final String SERVER_REST_PATH = "http://192.168.178.27:8080/Server/App/"; //Sören Home
 
-    public static final boolean LOGIN_REQUIRED = false;
+    public static final boolean LOGIN_REQUIRED = true;
 
 }
