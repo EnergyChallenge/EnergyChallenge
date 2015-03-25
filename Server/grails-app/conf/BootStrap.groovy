c543f8f3053c62c23945b7563cfc1f8bcd749079
Lennard Hammer <stu115635@mail.uni-kiel.de>
2015-03-25 16:47:32 +0100

    Added a Admin to Bootstrap and reviewd the code

--- Server/grails-app/conf/BootStrap.groovy
+++ Server/grails-app/conf/BootStrap.groovy
@@ -12,27 +12,32 @@ class BootStrap {
 
 	def init = { servletContext ->
 		def currentEnv = Environment.current
+		//define the user and admin role
+	    	def userRole = new Role(name: "user")
+	    	userRole.addToPermissions("*:*")
+	    	userRole.save(flush: true, failOnError: true);
+            	def adminRole = new Role(name: "admin")
+            	adminRole.addToPermissions("*:*")
+            	adminRole.save(flush: true, failOnError: true);
 
+		// initialization for productive use
+		InitService.initKlikRoles()
+		InitService.initKlikActivities()
+		InitService.initCauInstitutions()
 		if (currentEnv == Environment.DEVELOPMENT) {
 
 			// generate test data
-			def userRole = new Role(name: "user")
-			userRole.addToPermissions("*:*")
-			userRole.save();
-            def adminRole = new Role(name: "admin")
-            adminRole.addToPermissions("*:*")
-            adminRole.save();
-			Institute institute = new Institute(name: "none")
+			Institute institute = new Institute(name: "development")
 			institute.save()
 			def user = new User(email:"user@example.com", passwordHash: new Sha256Hash("password").toHex(), firstName: "Max", lastName: "Mustermann", institute: institute)
 			user.addToRoles(userRole)
 			user.save(flush: true)
+			def admin = new User(email:"admin@example.com", passwordHash: new Sha256Hash("password").toHex(), firstName: "Matilda", lastName: "Mustermann", institute: institute)
+			admin.addToRoles(adminRole)
+			admin.save(flush: true)
+
 			//TestService.saveSomeExampleData()
 			
-			// initialization for productive use
-			InitService.initKlikRoles()
-			InitService.initKlikActivities()
-			InitService.initCauInstitutions()
 			TestService.createAndSaveExampleUsersForExistingInstitutes(128)
 			TestService.createAndSaveCompletedActivitiesForExistingUsersAndActivities(1024)
 			TestService.createSomePageVisits(7,"/index")	
@@ -40,9 +45,22 @@ class BootStrap {
 			println "Running in Development Mode"
 		} else if (currentEnv == Environment.TEST) {
 			// do custom init for test here
+			Institute institute = new Institute(name: "test")
+			institute.save()
+			def user = new User(email:"user@example.com", passwordHash: new Sha256Hash("password").toHex(), firstName: "Max", lastName: "Mustermann", institute: institute)
+			user.addToRoles(userRole)
+			user.save(flush: true)
+			def admin = new User(email:"admin@example.com", passwordHash: new Sha256Hash("password").toHex(), firstName: "Matilda", lastName: "Mustermann", institute: institute)
+			admin.addToRoles(adminRole)
+			admin.save(flush: true)
 
 		} else if (currentEnv == Environment.PRODUCTION) {
 			// generate admin Accounts
+			Institute institute = Institute.get(0) //TODO find the right Institute
+			institute.save()
+			def admin = new User(email:"admin@example.com", passwordHash: new Sha256Hash("password").toHex(), firstName: "Matilda", lastName: "Mustermann", institute: institute)
+			admin.addToRoles(adminRole)
+			admin.save(flush: true)
 			
 		}
 	}
