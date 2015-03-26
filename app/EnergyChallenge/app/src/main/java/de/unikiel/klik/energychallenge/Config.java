9de2af16bb8cdbf46296b69824e267637c10e8ff
Sören Henning <stu114708@informatik.uni-kiel.de>
2015-03-26 11:27:19 +0100

    Comment Proposal in App - not done yet

--- app/EnergyChallenge/app/src/main/java/de/unikiel/klik/energychallenge/Config.java
+++ app/EnergyChallenge/app/src/main/java/de/unikiel/klik/energychallenge/Config.java
@@ -6,6 +6,7 @@ public class Config {
     //public static final String SERVER_REST_PATH = "http://192.168.0.83:8080/Server/App/"; //Lennard Notebook
     public static final String SERVER_REST_PATH = "http://192.168.0.102:8080/Server/App/"; //Samuel Notebook
     //public static final String SERVER_REST_PATH = "http://192.168.178.27:8080/Server/App/"; //Sören Home
+    //public static final String SERVER_REST_PATH = "http://luedtkegmbh.de/Soeren/Softwareprojekt/App/"; //Static serverdata
 
     public static final boolean LOGIN_REQUIRED = true;
 
