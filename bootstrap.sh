c785f791370d27518a7a2a64b00227c43104ba72
Samuel Barnes <sam@7software.co.uk>
2015-05-01 13:54:23 +0200

    Updating vagrant bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,10 @@
 apt-get update
-apt-get install -y openjdk-6-jdk curl groovy
+apt-get install -y openjdk-6-jdk curl groovy unzip
+
+echo "You have to login to vagrant at this point
+vagrant ssh
+curl -s get.gvmtool.net | bash
+source "/home/vagrant/.gvm/bin/gvm-init.sh"
+gvm install grails 2.4.4
+cd /vagrant/Server
+grails run-app"
