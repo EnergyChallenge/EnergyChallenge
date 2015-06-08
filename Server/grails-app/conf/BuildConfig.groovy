b03265115fe3665c42e1d95795b03881d6a67dc2
Sören Henning <bitbucket.web@soeren-henning.de>
2015-06-08 17:00:39 +0200

    try to set some jvm arguments

--- Server/grails-app/conf/BuildConfig.groovy
+++ Server/grails-app/conf/BuildConfig.groovy
@@ -1,3 +1,5 @@
+grails.tomcat.jvmArgs= ["-Xms256m",  "-Xmx1024m", "-XX:PermSize=256m", "-XX:MaxPermSize=512m"]
+
 grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
@@ -20,8 +22,6 @@ grails.project.fork = [
     // configure settings for the Console UI JVM
     console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
 ]
-
-grails.tomcat.jvmArgs= ["-Xms256m",  "-Xmx1024m", "-XX:PermSize=256m", "-XX:MaxPermSize=512m"]
 
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
